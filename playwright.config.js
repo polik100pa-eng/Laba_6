c6da8ddf7a986a84c37f59c0e1dd335c6408d54d
polina <polik100pa@gmail.com>
2026-04-20 01:24:07 +0300

    fix CI pipeline

--- playwright.config.js
+++ playwright.config.js
@@ -3,15 +3,12 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
 
-  // параллельный запуск (нужно для CI/CD задания)
   fullyParallel: true,
 
-  // CI поведение
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
 
-  // ===== РЕПОРТЫ (ВАЖНО ДЛЯ МЕТОДИЧКИ) =====
   reporter: [
     ['html', { outputFolder: 'playwright-report' }],
     ['line'],
@@ -19,16 +16,12 @@ export default defineConfig({
   ],
 
   use: {
-    // важно для твоего server.js
     baseURL: 'http://localhost:3000',
-
-    // стабильность тестов в CI
     trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
 
-  // браузеры (как в методичке)
   projects: [
     {
       name: 'chromium',
@@ -44,12 +37,5 @@ export default defineConfig({
     },
   ],
 
-  // локальный запуск сервера (очень желательно для CI)
-  webServer: {
-    command: 'npm start',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    // timeout: 120 * 1000,
-  },
+  // ❌ УБРАЛИ webServer — чтобы не было конфликтов в CI
 });
-
